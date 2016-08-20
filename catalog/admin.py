f574085790ecab80958f6c715c8e63c72449bd00
quantumke <nguruben@gmail.com>
2016-08-20 10:40:42 +0300

    setup catalog admin

---
+++ catalog/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from catalog.models import Product, Category
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'old_price', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    list_per_page = 50
+    ordering = ['-created_at']
+    search_fields = ['name','description','meta_keywords', 'meta_description']
+    # exclude = ('created_at', 'updated_at',)
+    prepopulated_fields = {'slug':('name',)}
+admin.site.register(Product, ProductAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at',)
+    list_display_links = ('name',)
+    list_per_page = 20
+    ordering = ['name']
+    search_fields = ['name', 'description', 'meta_keywords', 'meta_descrription']
+    # exclude = ('created_at', 'updated_at',)
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
